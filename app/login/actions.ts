bd048b35d83f4469bc50764e0794e6cf4a7dbb9b
cleenixwipes <0@chillen.de>
2024-08-06 22:01:02 +0200

    first commit

---
+++ app/login/actions.ts
@@ -0,0 +1,46 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+import { createClient } from '@/utils/supabase/server'
+
+export async function emaillogin(formData: FormData): Promise<void> {
+  const supabase = createClient()
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  const { error } = await supabase.auth.signInWithPassword(data)
+
+  if (error) {
+    redirect('/login?message= Could not authenticate user')
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/todos')
+}
+
+export async function signup(formData: FormData): Promise<void> {
+  const supabase = createClient()
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  const { error } = await supabase.auth.signUp(data)
+
+  if (error) {
+    redirect('/login?message=error signing up')
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/login')
+}
